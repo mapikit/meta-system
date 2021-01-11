4548ede07539322bbbaadd0b2626e43e1fd96538
Fábio Meneses Junior <fabiomenesesjr@gmail.com>
2021-01-11 08:54:49 -0300

    Define Type Interface For BOPS json data

---
+++ src/configuration-de-serializer/domain/business-operations.ts
@@ -0,0 +1,56 @@
+import { JsonTypes } from "@api/common/json-types";
+
+export interface BusinessOperations {
+  name : string;
+  inputs : BopsInput[];
+  outputs : BopsOutput[];
+  route : string;
+  constants : BopsConstant[];
+  configuration : BopsConfigurationEntry[];
+  usedAsRoute : boolean;
+  customObjects : BopsCustomObject[];
+}
+
+export interface BopsInput {
+  name : string;
+  type : string;
+  localization : "uri" | "body";
+}
+
+export interface BopsOutput {
+  name : string;
+  type : string;
+  statusCode : number;
+}
+
+export interface BopsConstant {
+  name : string;
+  type : JsonTypes;
+}
+
+export interface BopsConfigurationEntry {
+  moduleRepo : string;
+  key : number;
+  inputsSource : InputsSource[];
+  nextFunctions : NextFunctions[];
+}
+
+export interface InputsSource {
+  source : string;
+  target : string;
+}
+
+export interface NextFunctions {
+  nextKey : string;
+  branch ?: string;
+}
+
+export interface BopsCustomObject {
+  name : string;
+  properties : BopsCustomObjectProperty[];
+}
+
+export interface BopsCustomObjectProperty {
+  name : string;
+  type : string;
+}
