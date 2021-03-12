fe60e0cdcad7281b3cf7a8227e8ef64c34e5bd99
Fábio Meneses Junior <fabiomenesesjr@gmail.com>
2021-03-11 21:45:53 -0300

    #26 - Meta Function Json Validator Wrapper Tests

--- test/bops-functions/function-configuration-validator.spec.ts
+++ test/bops-functions/function-configuration-validator.spec.ts
@@ -1,17 +1,16 @@
 require("module-alias/register");
 import { FunctionFileSystem } from "@api/bops-functions/installation/function-file-system";
+import { MetaFunctionDescriptionValidation } from "@api/bops-functions/installation/functions-configuration-validation";
 import { FunctionsInstaller, ModuleKind } from "@api/bops-functions/installation/functions-installer";
 import { expect } from "chai";
 import Path from "path";
 
-describe("File System BOps", () => {
+describe("BOps Function Configuration Validator", () =>{
   const testFunctionName = "bops-function-hello-world";
   const testFunctionVersion = "1.0.0";
   const functionInstallLocation = "./test-functions";
   const workingDirectory = process.cwd();
   const configurationFileName = "meta-function.json";
-  const mainFunctionName = "main";
-  const entrypoint = "dist/index.js";
 
   const installationHandler = new FunctionsInstaller(
     Path.join(workingDirectory, functionInstallLocation),
@@ -21,29 +20,38 @@ describe("File System BOps", () => {
     await installationHandler.purgePackages();
   });
 
-  it("Retrieve function configuration file", async () => {
+  it("Validates successfully a valid meta-function.json", async () => {
     await installationHandler.install(testFunctionName, testFunctionVersion, ModuleKind.NPM);
 
     const functionFileSystem = new FunctionFileSystem(
       workingDirectory, functionInstallLocation, configurationFileName,
     );
 
-    const result = await functionFileSystem.getFunctionDescriptionFile(testFunctionName);
+    const metaFunctionContent = await functionFileSystem
+      .getFunctionDescriptionFile(testFunctionName);
 
-    expect(result).to.contain(`"functionName": "${testFunctionName}"`);
-    expect(result).to.contain(`"version": "${testFunctionVersion}"`);
+    const validator = new MetaFunctionDescriptionValidation(metaFunctionContent);
+
+    const execution = () : void => validator.validate();
+
+    expect(execution).to.not.throw;
   });
 
-  it("Imports Main Function from file", async () => {
+  it("Validates successfully a valid meta-function.json", async () => {
     await installationHandler.install(testFunctionName, testFunctionVersion, ModuleKind.NPM);
 
     const functionFileSystem = new FunctionFileSystem(
       workingDirectory, functionInstallLocation, configurationFileName,
     );
 
-    const mainFunction = await functionFileSystem
-      .importMain(testFunctionName, entrypoint, mainFunctionName);
+    const metaFunctionContent = await functionFileSystem
+      .getFunctionDescriptionFile(testFunctionName);
 
-    expect(typeof mainFunction).to.be.equal("function");
+    const validator = new MetaFunctionDescriptionValidation(metaFunctionContent);
+
+    validator.validate();
+
+    expect(validator.getFunctionConfiguration().functionName).to.be.equal(testFunctionName);
+    expect(validator.getFunctionConfiguration().version).to.be.equal(testFunctionVersion);
   });
 });
