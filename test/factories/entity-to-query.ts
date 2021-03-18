c42fc52fdd3f749785062c75481d5232b5200af9
homemmakako <homemmakako@gmail.com>
2021-03-18 18:41:30 -0300

    Improved readability

--- test/factories/entity-to-query.ts
+++ test/factories/entity-to-query.ts
@@ -6,20 +6,14 @@ export const entityToQuery = (entity : object) : string => {
   return query.substring(1);
 };
 
-
 const typeCreation = {
   string: (string : string, propName : string) : string => `&${propName}=${string}`,
   number: (number : number, propName : string) : string => `&${propName}=${number}`,
   boolean: (bool : boolean, propName : string) : string => `&${propName}=${bool.toString()}`,
-  // eslint-disable-next-line max-lines-per-function
   object: (object : object, propName ?: string) : string => {
-    if(object instanceof Array && typeof object[0] === "object") {
-      let query = "";
-      object.forEach((obj, index) => {
-        query += typeCreation.object(obj, `${propName}[${index}]`);
-      });
-      return  query;
-    }
+    // To javascript, both Arrays and Dates are considered Objects. Because of this, it is
+    // required to diferentiate between regular Objects, Arrays and Dates.
+    if(isArrayOfObjects(object)) return arrayOfObjectsToQuery(object as Array<object>, propName);
     if(object instanceof Array) return `&${propName}[]=` + object.join(`&${propName}[]=`);
     if(object instanceof Date) return `&${propName}=${object.toISOString()}`;
 
@@ -31,3 +25,15 @@ const typeCreation = {
     return query;
   },
 };
+
+function isArrayOfObjects (object : object) : boolean {
+  return object instanceof Array && typeof object[0] === "object";
+}
+
+function arrayOfObjectsToQuery (array : Array<object>, propName ?: string) : string {
+  let query = "";
+  array.forEach((object, index) => {
+    query += typeCreation.object(object, `${propName}[${index}]`);
+  });
+  return  query;
+}
