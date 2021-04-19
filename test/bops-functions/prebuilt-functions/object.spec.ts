e0b9f17351863fc49da1b30f6dacdeda7665701e
Fábio Meneses Junior <fabiomenesesjr@gmail.com>
2021-04-19 08:55:49 -0300

    #91 Object Keys BOPs function

--- test/bops-functions/prebuilt-functions/object.spec.ts
+++ test/bops-functions/prebuilt-functions/object.spec.ts
@@ -1,25 +1,46 @@
 import { createObjectBopsFunction } from "@api/bops-functions/prebuilt-functions/object/create";
+import { getObjectKeysBopsFunction } from "@api/bops-functions/prebuilt-functions/object/keys";
 import { expect } from "chai";
 
 describe("Object Bops Functions", () => {
-  it("Creates an object with the given key and values", () => {
-    const key = "name";
-    const value = "Jonathan";
+  describe("Object Create", () => {
+    it("Creates an object with the given key and values", () => {
+      const key = "name";
+      const value = "Jonathan";
 
-    const result = createObjectBopsFunction({ key, value });
+      const result = createObjectBopsFunction({ key, value });
 
-    expect(result).to.be.deep.equal({ created: { name: "Jonathan" } });
+      expect(result).to.be.deep.equal({ created: { name: "Jonathan" } });
+    });
+
+    it("Creates an empty object", () => {
+      const result = createObjectBopsFunction({});
+
+      expect(result).to.be.deep.equal({ created: {} });
+    });
+
+    it("Creates an empty object if the key is not passed", () => {
+      const result = createObjectBopsFunction({ value: "Test" });
+
+      expect(result).to.be.deep.equal({ created: {} });
+    });
   });
 
-  it("Creates an empty object", () => {
-    const result = createObjectBopsFunction({});
+  describe("Keys", () => {
+    it("Gets keys of the object successfully", () => {
+      const object = { name: "John", lastName: "Doe", age: 17 };
 
-    expect(result).to.be.deep.equal({ created: {} });
-  });
+      const result = getObjectKeysBopsFunction({ object });
 
-  it("Creates an empty object if the key is not passed", () => {
-    const result = createObjectBopsFunction({ value: "Test" });
+      expect(result).to.be.deep.equal({ result: [ "name", "lastName", "age" ] });
+    });
 
-    expect(result).to.be.deep.equal({ created: {} });
+    it("Converts Non numeric Keys to String", () => {
+      const object = { name: "John", lastName: "Doe", age: 17, 6: null };
+
+      const result = getObjectKeysBopsFunction({ object });
+
+      expect(result).to.be.deep.equal({ result: [ "6" ,"name", "lastName", "age" ] });
+    });
   });
 });
