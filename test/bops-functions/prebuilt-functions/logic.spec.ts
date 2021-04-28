8f32a671da4c263a8b343c54d796ab3307e83172
Fábio Meneses Junior <fabiomenesesjr@gmail.com>
2021-04-27 22:21:09 -0300

    [ #107 ] Higher or equal to BOPs function

--- test/bops-functions/prebuilt-functions/logic.spec.ts
+++ test/bops-functions/prebuilt-functions/logic.spec.ts
@@ -1,3 +1,4 @@
+import { higherOrEqualToBopsFunction } from "@api/bops-functions/prebuilt-functions/logic/higher-or-equal-to";
 import { lowerOrEqualToBopsFunction } from "@api/bops-functions/prebuilt-functions/logic/lower-or-equal-to";
 import { lowerThanBopsFunction } from "@api/bops-functions/prebuilt-functions/logic/lower-than";
 import { expect } from "chai";
@@ -33,7 +34,7 @@ describe("Logic BOPs function", () => {
   });
 
   describe("Lower Or Equal To", () => {
-    it("Verifies If returns true for a lower comparison (A < B)", () => {
+    it("Verifies If returns true for a lower comparison (A <= B)", () => {
       const A = 1;
       const B = 7;
 
@@ -42,7 +43,7 @@ describe("Logic BOPs function", () => {
       expect(result).to.be.deep.equal({ isLowerOrEqual: true });
     });
 
-    it("Verifies If returns false for a higher comparison (A < B)", () => {
+    it("Verifies If returns false for a higher comparison (A <= B)", () => {
       const A = 99301;
       const B = 1;
 
@@ -51,13 +52,42 @@ describe("Logic BOPs function", () => {
       expect(result).to.be.deep.equal({ isNotLowerOrEqual: true });
     });
 
-    it("Verifies If returns true for an equal comparison (A < B)", () => {
+    it("Verifies If returns true for an equal comparison (A <= B)", () => {
       const A = Math.PI;
       const B = A;
 
       const result = lowerOrEqualToBopsFunction({ A, B });
 
       expect(result).to.be.deep.equal({ isLowerOrEqual: true });
+    });
+  });
+
+  describe("Higher Or Equal To", () => {
+    it("Verifies If returns false for a lower comparison (A >= B)", () => {
+      const A = 1;
+      const B = 7;
+
+      const result = higherOrEqualToBopsFunction({ A, B });
+
+      expect(result).to.be.deep.equal({ isNotHigherOrEqual: true });
+    });
+
+    it("Verifies If returns true for a higher comparison (A >= B)", () => {
+      const A = 99301;
+      const B = 1;
+
+      const result = higherOrEqualToBopsFunction({ A, B });
+
+      expect(result).to.be.deep.equal({ isHigherOrEqual: true });
+    });
+
+    it("Verifies If returns true for an equal comparison (A >= B)", () => {
+      const A = Math.PI;
+      const B = A;
+
+      const result = higherOrEqualToBopsFunction({ A, B });
+
+      expect(result).to.be.deep.equal({ isHigherOrEqual: true });
     });
   });
 });
