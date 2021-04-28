a951bd478e768d1478d39369650f87e3bac32912
Fábio Meneses Junior <fabiomenesesjr@gmail.com>
2021-04-27 22:42:14 -0300

    [ #106 ] Higher than BOps function

--- test/bops-functions/prebuilt-functions/logic.spec.ts
+++ test/bops-functions/prebuilt-functions/logic.spec.ts
@@ -1,4 +1,5 @@
 import { higherOrEqualToBopsFunction } from "@api/bops-functions/prebuilt-functions/logic/higher-or-equal-to";
+import { higherThanBopsFunction } from "@api/bops-functions/prebuilt-functions/logic/higher-than";
 import { lowerOrEqualToBopsFunction } from "@api/bops-functions/prebuilt-functions/logic/lower-or-equal-to";
 import { lowerThanBopsFunction } from "@api/bops-functions/prebuilt-functions/logic/lower-than";
 import { expect } from "chai";
@@ -88,6 +89,35 @@ describe("Logic BOPs function", () => {
       const result = higherOrEqualToBopsFunction({ A, B });
 
       expect(result).to.be.deep.equal({ isHigherOrEqual: true });
+    });
+  });
+
+  describe("Higher than", () => {
+    it("Verifies If returns false for a lower comparison (A > B)", () => {
+      const A = 1;
+      const B = 7;
+
+      const result = higherThanBopsFunction({ A, B });
+
+      expect(result).to.be.deep.equal({ isNotHigher: true });
+    });
+
+    it("Verifies If returns true for a higher comparison (A > B)", () => {
+      const A = 99301;
+      const B = 1;
+
+      const result = higherThanBopsFunction({ A, B });
+
+      expect(result).to.be.deep.equal({ isHigher: true });
+    });
+
+    it("Verifies If returns false for an equal comparison (A > B)", () => {
+      const A = Math.PI;
+      const B = A;
+
+      const result = higherThanBopsFunction({ A, B });
+
+      expect(result).to.be.deep.equal({ isNotHigher: true });
     });
   });
 });
