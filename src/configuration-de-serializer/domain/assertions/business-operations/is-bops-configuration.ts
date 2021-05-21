e2622d941cb5ced16019af8e2d98aed9b4bede24
homemmakako <homemmakako@gmail.com>
2021-05-21 19:29:41 -0300

    Updated verification to new model (Using inputsSources instead of nextFunctions)

--- src/configuration-de-serializer/domain/assertions/business-operations/is-bops-configuration.ts
+++ src/configuration-de-serializer/domain/assertions/business-operations/is-bops-configuration.ts
@@ -1,5 +1,4 @@
-import { BopsConfigurationEntry, InputsSource, NextFunctions } from
-  "@api/configuration-de-serializer/domain/business-operations-type";
+import { BopsConfigurationEntry, InputsSource } from "@api/configuration-de-serializer/domain/business-operations-type";
 import { isType } from "@api/configuration-de-serializer/domain/assertions/is-type";
 
 function isInputsSource (input : unknown) : asserts input is InputsSource {
@@ -17,22 +16,6 @@ function isInputsSource (input : unknown) : asserts input is InputsSource {
   });
 }
 
-function isNextFunctions (input : unknown) : asserts input is NextFunctions {
-  if (!Array.isArray(input)) {
-    throw Error("Business Operation Configuration with wrong type found: \"nextFunctions\" should be an Array");
-  }
-
-  const inputAssertion = input as NextFunctions[];
-
-  inputAssertion.forEach((nextFunction) => {
-    isType("number", "nextKey must be a number", nextFunction.nextKey);
-
-    if (nextFunction.branch !== undefined) {
-      isType("string", "If branch is present must be a string", nextFunction.branch);
-    }
-  });
-}
-
 export function isBopsConfigurationEntry (input : unknown) : asserts input is BopsConfigurationEntry[] {
   if (!Array.isArray(input)) {
     throw Error("Business Operation Configuration with wrong type found: configuration should be an Array");
@@ -44,10 +27,5 @@ export function isBopsConfigurationEntry (input : unknown) : asserts input is Bo
     isType("string", "\"moduleRepo\" must be a string", config.moduleRepo);
     isType("number", "\"key\" must be a string", config.key);
     isInputsSource(config.inputsSource);
-
-    // Not the last module of the chain
-    if (config.moduleRepo.charAt(0) !== "%") {
-      isNextFunctions(config.nextFunctions);
-    }
   });
 }
