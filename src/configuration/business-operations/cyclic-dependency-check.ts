2f7dcff1999b8c5c9b0c8b8aa3d343232d07bc3d
Fábio Meneses Junior <flgmjr@gmail.com>
2021-06-29 22:26:33 -0300

    [ #186 ] Check for cyclic dependencies between BOps

---
+++ src/configuration/business-operations/cyclic-dependency-check.ts
@@ -0,0 +1,48 @@
+import { BusinessOperations } from "./business-operations-type";
+
+export class BopsCyclicDependencyCheck {
+  public constructor (
+    private businessOperations : BusinessOperations[],
+  ) {}
+
+  public checkAllBops () : void {
+    this.businessOperations.forEach((bops) => {
+      this.checkBopDependencyChain(bops.name);
+    });
+  }
+
+  public checkBopDependencyChain (bopsName : string, requireChain : string[] = []) : void {
+    if (requireChain.includes(bopsName)) {
+      throw Error("Cannot run a BOp with a cyclic dependecy: " + requireChain.join(", "));
+    }
+
+    requireChain.push(bopsName);
+
+    const currentDependencies = this.getBopsDependencies(bopsName);
+    currentDependencies.forEach((dependency) => {
+      this.checkBopDependencyChain(dependency, requireChain);
+    });
+  }
+
+  private getBopsDependencies (bopsName : string) : string[] {
+    const result = [];
+
+    const bops = this.getBopsByName(bopsName);
+
+    if (bops === undefined) {
+      return [];
+    }
+
+    bops.configuration.forEach((config) => {
+      if (config.moduleRepo.charAt(0) === "+") {
+        result.push(config.moduleRepo.slice(1));
+      }
+    });
+
+    return result;
+  }
+
+  private getBopsByName (bopsName : string) : BusinessOperations | undefined {
+    return this.businessOperations.find((bops) => bops.name === bopsName);
+  }
+}
