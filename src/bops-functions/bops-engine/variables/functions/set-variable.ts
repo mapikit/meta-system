794772aa5344c4c73f84188cbaa24c55ca633a57
homemmakako <homemmakako@gmail.com>
2021-07-06 17:04:46 -0300

    Variable functions no loger throw Instead simply return a errorMessage

--- src/bops-functions/bops-engine/variables/functions/set-variable.ts
+++ src/bops-functions/bops-engine/variables/functions/set-variable.ts
@@ -3,10 +3,12 @@ import { InternalMetaFunction } from "@api/bops-functions/internal-meta-function
 export function setVariableFunction (input : { variableName : string; value : unknown }) : unknown {
   const foundVariable = this.variables[input.variableName];
   const isValidType = typeof input.value === foundVariable.type;
-  if(!isValidType) throw new Error("asdasd");
+  if(!isValidType) {
+    return { errorMessage: `Type "${typeof input.value}" is not compatible with "${foundVariable.type}"` }
+  }
 
   foundVariable.value = input.value;
-  return { newValue: this.variables[input.variableName] };
+  return { newValue: this.variables[input.variableName].value };
 }
 
 export const setVariableFunctionInformation : InternalMetaFunction = {
@@ -18,6 +20,7 @@ export const setVariableFunctionInformation : InternalMetaFunction = {
     value: { type: "any", required: true },
   },
   outputData: {
-    newValue: { type: "any", required: true },
+    newValue: { type: "any", required: false },
+    errorMessage: { type: "string", required: false },
   },
 };
