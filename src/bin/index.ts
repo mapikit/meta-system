626a190c5642f1c0a0b374fae1ae39c338fe0b64
homemmakako <homemmakako@gmail.com>
2021-07-21 19:35:47 -0300

    Added the ability to stop/start (reload) system

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,9 +1,46 @@
 #!/usr/bin/env node
 import { SystemSetup } from "../bootstrap/system-setup";
+import Path from "path";
+import fs from "fs";
 
+const fileLocation = process.argv[2];
+
+const relativePath = Path.join(process.cwd(), fileLocation);
+const absolutePath = Path.join(fileLocation);
+const setupProcess = new SystemSetup();
+
+
+// eslint-disable-next-line max-lines-per-function
 const main = async () : Promise<void> => {
-  const setupProcess = new SystemSetup();
   await setupProcess.execute();
+
+  process.stdin.on("data", (data) => {
+    if(data.toString().includes("rs")) {
+      console.log("Restarting System...");
+      void setupProcess.stop().then(() => {
+        void setupProcess.execute();
+      });
+    }
+  });
+
+  process.on("SIGINT", () => {
+    console.log("\nShutting down to to user input (SIGINT)");
+    void setupProcess.stop().then(() => {
+      process.exit(130);
+    });
+  });
+
+  if (process.argv.includes("--dev")) {
+    for (const path of [relativePath, absolutePath]) {
+      if(fs.existsSync(path)) {
+        fs.watchFile(path, () => {
+          void setupProcess.stop()
+            .then(void setupProcess.execute());
+        });
+        break;
+      };
+    }
+  }
 };
 
 main().catch((e) => { throw e; });
