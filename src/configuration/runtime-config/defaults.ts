eb9ba250a1479d410caf177607cd5eee085fadc5
Fábio Meneses Junior <flgmjr@gmail.com>
2021-09-20 22:36:52 -0300

    remove custom folder for installing functions locally

--- src/configuration/runtime-config/defaults.ts
+++ src/configuration/runtime-config/defaults.ts
@@ -1,7 +1,10 @@
 import Path from "path";
 
 export const runtimeDefaults = {
-  externalFunctionInstallFolder: Path.join(process.cwd(), "./external-functions"),
+  // We wanted to have a separate folder for external functions, but unfortunately,
+  // Node.js can only solve modules from `node_modules` and nothing else
+  // And no - there is no way to change it. :(
+  externalFunctionInstallFolder: Path.join(process.cwd(), "./node_modules"),
   externalFunctionConfigFileName: "meta-function.json",
   externalPackageConfigFileName: "meta-package.json",
   externalProtocolConfigFileName: "meta-protocol.json",
