856af82f4eca52227659591c023d6588d27097e3
Fábio Meneses Junior <flgmjr@gmail.com>
2022-01-03 18:53:47 -0300

    Remove lint disable

--- src/bops-functions/installation/functions-installer.ts
+++ src/bops-functions/installation/functions-installer.ts
@@ -12,7 +12,6 @@ export class FunctionsInstaller {
 
   private installationManager = new DependenciesManager(this.functionsFolder);
 
-  // eslint-disable-next-line max-lines-per-function
   public async install (moduleName : string, version : string, kind : ModuleKind) : Promise<void> {
     if (kind === ModuleKind.GITHUB) {
       throw Error("Not Implemented");
