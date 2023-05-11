95442ba4d9da6892595ac58a9e72304e690553b9
Fábio Meneses Junior <flgmjr@gmail.com>
2023-05-11 00:21:58 -0300

    upgrade library object definition

--- src/configuration/configuration-definition.ts
+++ src/configuration/configuration-definition.ts
@@ -1,54 +1,56 @@
-{
+export const configurationTypeDefinition = {
   "name": { "type": "string" },
   "version": { "type": "string" },
   "envs": { "type": "array", "subtype": {
     "key": { "type": "string" },
-    "value": { "type": "string" }
-  }},
+    "value": { "type": "string" },
+  } },
   "schemas": { "type": "array", "subtype": {
     "name": { "type": "string" },
     "identifier": { "type": "string", "required": true },
-    "format": { "type": "cloudedObject" }
-  }},
+    "format": { "type": "__%objdef%__" },
+  } },
   "addons": { "type": "array", "subtype": {
     "version": { "type": "string" },
     "source": { "type": "string" },
     "collectStrategy": { "type": "enum", "subtype": ["npm", "file", "url"] },
-    "configuration": { "type": "cloudedObject" }
-  }},
+    "configuration": { "type": "cloudedObject" },
+  } },
   "businessOperations": { "type": "array", "subtype": {
     "ttl": { "type": "number" },
     "name": { "type": "string" },
-    "input": { "type": "cloudedObject" },
-    "output": { "type": "cloudedObject" },
+    "input": { "type": "__%objdef%__" },
+    "output": { "type": "__%objdef%__" },
     "identifier": { "type": "string" },
     "constants": { "type": "array", "subtype": {
       "name": { "type": "string" },
       "type": { "type": "enum", "subtype": [ "string", "number", "boolean", "date", "object" ] },
-      "value": { "type": "any" }
-    }},
+      "value": { "type": "any" },
+    } },
     "variables": { "type": "array", "subtype": {
       "name": { "type": "string" },
-      "type": { "type": "enum", "subtype": [ "string", "number", "boolean", "date", "object", "any", "cloudedObject", "function" ] },
-      "initialValue": { "type": "any" }
-    }},
+      "type": { "type": "enum", "subtype": [
+        "string", "number", "boolean", "date", "object", "any", "cloudedObject", "function",
+      ] },
+      "initialValue": { "type": "any" },
+    } },
     "configuration": { "type": "array", "subtype": {
       "moduleType": {
-        "type": "enum", 
-        "subtype": [ "internal", "addon", "bop", "variable", "output" ] 
+        "type": "enum",
+        "subtype": [ "internal", "addon", "bop", "variable", "output" ],
       },
       "moduleName": { "type": "string" },
       "modulePackage": { "type": "string" },
       "key": { "type": "number" },
       "dependencies": { "type": "array", "subtype": {
         "origin": [
-          { "type": "enum", "subtype": ["constants", "variables", "input", "env" ] }, 
-          { "type": "number" }
+          { "type": "enum", "subtype": ["constants", "variables", "input", "env" ] },
+          { "type": "number" },
         ],
         "originPath": { "type": "string" },
-        "targetPath": { "type": "string" }
-      }}
-    }}
-    
-  }}
-}
+        "targetPath": { "type": "string" },
+      } },
+    } },
+
+  } },
+};
