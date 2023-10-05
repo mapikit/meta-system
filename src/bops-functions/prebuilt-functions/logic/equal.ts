97e6c20cb57c08f9450b358f3052cdb8b6daa4df
homemmakako <homemmakako@gmail.com>
2023-10-05 20:50:17 -0300

    Made isEqual bop function browser compatible

--- src/bops-functions/prebuilt-functions/logic/equal.ts
+++ src/bops-functions/prebuilt-functions/logic/equal.ts
@@ -1,10 +1,11 @@
 import { InternalMetaFunction } from "../../internal-meta-function.js";
-// TODO Possibly make this browser compatible (impossible with util import)
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const isEqualToBopsFunction = async (input : { A : any; B : any }) : Promise<unknown> => {
-  const { isDeepStrictEqual } = await import("util");
-  const isEqual = isDeepStrictEqual(input.A, input.B);
+  if(typeof input.A === "object" && typeof input.B === "object") {
+    return { isEqual: JSON.stringify(input.A) === JSON.stringify(input.B) };
+  }
 
+  const isEqual = input.A === input.B;
   return ({ isEqual });
 };
 
